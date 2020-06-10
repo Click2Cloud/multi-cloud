9c9f02fc445f9804920964096b053a999442f13b
Click2Cloud-Gamma <45617130+Click2Cloud-Gamma@users.noreply.github.com>
2020-06-10 11:34:57 +0530

    to make noauth compatible

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -22,10 +22,10 @@ import (
 	"github.com/micro/go-plugins/registry/mdns/v2"
 	"github.com/opensds/multi-cloud/api/pkg/backend"
 	"github.com/opensds/multi-cloud/api/pkg/dataflow"
-	"github.com/opensds/multi-cloud/api/pkg/filters/auth"
+	//"github.com/opensds/multi-cloud/api/pkg/filters/auth"
 	"github.com/opensds/multi-cloud/api/pkg/filters/context"
 	"github.com/opensds/multi-cloud/api/pkg/filters/logging"
-	"github.com/opensds/multi-cloud/api/pkg/filters/signature/signer"
+	//"github.com/opensds/multi-cloud/api/pkg/filters/signature/signer"
 	"github.com/opensds/multi-cloud/api/pkg/s3"
 	"github.com/opensds/multi-cloud/api/pkg/utils/obs"
 	log "github.com/sirupsen/logrus"
@@ -68,7 +68,7 @@ func main() {
 
 		s3ws.Filter(logging.FilterFactory())
 		s3ws.Filter(context.FilterFactory())
-		s3ws.Filter(signer.FilterFactory())
+		//s3ws.Filter(signer.FilterFactory())
 		s3.RegisterRouter(s3ws)
 
 		wc.Add(s3ws)
@@ -84,7 +84,7 @@ func main() {
 		// add filter for authentication context
 		ws.Filter(logging.FilterFactory())
 		ws.Filter(context.FilterFactory())
-		ws.Filter(auth.FilterFactory())
+		//ws.Filter(auth.FilterFactory())
 		wc.Add(ws)
 	}
 
