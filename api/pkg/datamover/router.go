ce10d6c0cf63e5a394dae9e2d096def649c2813e
Saurabh Gupta <saurabh.gupta@click2cloud.net>
2020-10-27 16:01:33 +0530

    datamover Register router

--- api/pkg/datamover/router.go
+++ api/pkg/datamover/router.go
@@ -7,10 +7,8 @@ import (
 
 func RegisterRouter(ws *restful.WebService) {
 	handler := NewAPIService(client.DefaultClient)
-	ws.Route(ws.POST("/jobs/{id}/abort").To(handler.AbortJob)).
-		Doc("Cancel job")
-	ws.Route(ws.POST("/jobs/{id}/pause").To(handler.PauseJob)).
-		Doc("Pause job")
+	ws.Route(ws.POST("/jobs/{id}/abort").To(handler.AbortJob)).Doc("Cancel job")
+	ws.Route(ws.POST("/jobs/{id}/pause").To(handler.PauseJob)).Doc("Pause job")
 	//ws.Route(ws.PUT("/{tenantId}/objectlifecycle").To(handler.ObjectLifeCycle)).
 	//	Doc("Pause job")
 }
