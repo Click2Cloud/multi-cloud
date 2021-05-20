fc46783543fb2a05a1a1c060331fe00bc4bfbab1
Click2Cloud-SGupta <saurabh.gupta@click2cloud.net>
2021-05-20 15:53:05 +0530

    move to glacier bug fix test

---
+++ api/pkg/s3/routemovetoglacier.go
@@ -0,0 +1,19 @@
+package s3
+
+import (
+	"github.com/emicklei/go-restful"
+	"github.com/opensds/multi-cloud/api/pkg/filters/signature"
+	log "github.com/sirupsen/logrus"
+	//"github.com/prometheus/common/log"
+)
+
+func (s *APIService) RouteMoveToGlacier(request *restful.Request, response *restful.Response) {
+	err := signature.PayloadCheck(request, response)
+	if err != nil {
+		WriteErrorResponse(response, request, err)
+		return
+	}
+	log.Info("Route to move to Glacier")
+	s.MoveToGlacier(request, response)
+
+}
