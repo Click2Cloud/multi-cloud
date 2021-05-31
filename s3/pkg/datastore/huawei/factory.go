f93a07dda412c171fa9b25938a8b5db69cec70f0
Click2Cloud-SGupta <saurabh.gupta@click2cloud.net>
2021-05-31 15:06:29 +0530

    testing AWS sdk for huawei

--- s3/pkg/datastore/huawei/factory.go
+++ s3/pkg/datastore/huawei/factory.go
@@ -1,28 +1,18 @@
 package hws
 
 import (
-	"github.com/opensds/multi-cloud/api/pkg/utils/obs"
 	"github.com/opensds/multi-cloud/backend/pkg/utils/constants"
 	backendpb "github.com/opensds/multi-cloud/backend/proto"
+	"github.com/opensds/multi-cloud/s3/pkg/datastore/aws"
 	"github.com/opensds/multi-cloud/s3/pkg/datastore/driver"
 )
 
 type HWObsDriverFactory struct {
 }
 
-func (cdf *HWObsDriverFactory) CreateDriver(backend *backendpb.BackendDetail) (driver.StorageDriver, error) {
-	endpoint := backend.Endpoint
-	AccessKeyID := backend.Access
-	AccessKeySecret := backend.Security
-
-	client, err := obs.New(AccessKeyID, AccessKeySecret, endpoint)
-	if err != nil {
-		return nil, err
-	}
-
-	adap := &OBSAdapter{backend: backend, client: client}
-
-	return adap, nil
+func (factory *HWObsDriverFactory) CreateDriver(backend *backendpb.BackendDetail) (driver.StorageDriver, error) {
+	awss3Fac := &aws.AwsS3DriverFactory{}
+	return awss3Fac.CreateDriver(backend)
 }
 
 func init() {
